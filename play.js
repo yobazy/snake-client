21dbba1f1798c33337369e9110356b4ee5e33b60
Bazil Khan <king.bazil.khan@gmail.com>
2022-05-11 22:28:21 +0000

    client for snake

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541'
+  });
+  conn.on("data", (messageFromServer) => {
+    console.log('Server says:', messageFromServer)
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
